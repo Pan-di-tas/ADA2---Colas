7ffee449f92d469b5aa15c7f06fedea2f38f4759
Abdiel <maganaabdiel02@gmail.com>
2024-10-18 21:04:00 -0600

    Update Suma_Colas.py

--- Suma_Colas.py
+++ Suma_Colas.py
@@ -20,34 +20,44 @@ class Cola:
         if not self.Vacia():
             return self.items[0]
         return None
+    
+    def Pedir_Datos(self):
+        n_a = int(input("Ingrese la cantidad de elementos para la Cola A y B: "))
+        print("Ingrese los valores para la Cola A:")
+        for _ in range(n_a):
+            valor = int(input())
+            cola_a.encolar(valor)
+            print(f"Valor {valor} encolado en Cola A")
+            print(f"Cola a: {cola_a.items}")
 
-def sumar_colas(cola_a, cola_b):
-    cola_resultado = Cola()
-    while not cola_a.Vacia() and not cola_b.Vacia():
-        valor_a = cola_a.desencolar()
-        valor_b = cola_b.desencolar()
-        cola_resultado.encolar(valor_a + valor_b)
-    return cola_resultado
+        print("\nIngrese los valores para la Cola B:")
+        for _ in range(n_a):
+            valor = int(input())
+            cola_b.encolar(valor)
+            print(f"Valor {valor} encolado en Cola B")
+            print(f"Cola b: {cola_b.items}")
+
+    def sumar_colas(self, cola_a, cola_b):
+        while not cola_a.Vacia() and not cola_b.Vacia():
+            valor_a = cola_a.desencolar()
+            valor_b = cola_b.desencolar()
+            print(f"\nValores a: {valor_a} y valor b: {valor_b} desencolados")
+            print(f"Cola a: {cola_a.items} y Cola b: {cola_b.items}")
+            self.valor_suma = valor_a + valor_b
+            print(f"Suma de los valores a y b: {self.valor_suma}")
+            cola_resultado.encolar(valor_a + valor_b)
+            print(f"\nEncolando {self.valor_suma} en {cola_resultado.items}")
+        return cola_resultado
+    
+    def restriccion(self):
+        print("\nCola Resultado:")
+        while not cola_resultado.Vacia():
+            print(cola_resultado.desencolar())
 
 cola_a = Cola()
 cola_b = Cola()
+cola_resultado = Cola()
 
-n_a = int(input("Ingrese la cantidad de elementos para la Cola A: "))
-print("Ingrese los valores para la Cola A:")
-for _ in range(n_a):
-    valor = int(input())
-    cola_a.encolar(valor)
-    print(f"Valor {valor} encolado en Cola A")
-
-n_b = int(input("Ingrese la cantidad de elementos para la Cola B: "))
-print("Ingrese los valores para la Cola B:")
-for _ in range(n_b):
-    valor = int(input())
-    cola_b.encolar(valor)
-    print(f"Valor {valor} encolado en Cola B")
-
-cola_resultado = sumar_colas(cola_a, cola_b)
-
-print("\nCola Resultado:")
-while not cola_resultado.Vacia():
-    print(cola_resultado.desencolar())
+cola_resultado.Pedir_Datos()
+cola_resultado.sumar_colas(cola_a,cola_b)
+cola_resultado.restriccion()
